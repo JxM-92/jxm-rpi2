ac535e7ce250304787d31c89edbd010dcf5f9880
Joe X <jamedinao@gmail.com>
2016-03-04 17:40:46 -0500

    Update chromium-browser-48.0.2564.82_installer-2016.03.03_armhf.sh

--- chromium-browser-48.0.2564.82_installer-2016.03.03_armhf.sh
+++ chromium-browser-48.0.2564.82_installer-2016.03.03_armhf.sh
@@ -8,7 +8,7 @@ wget https://dl.dropboxusercontent.com/u/87113035/chromium-codecs-ffmpeg-extra_4
 wget http://steinerdatenbank.de/software/kweb-1.6.9.tar.gz
 clear && echo $"Installing Dependencies...\n"
 sudo apt-get update && sudo apt-get -y upgrade
-sudo apt-get -y install gdebi gconf-service libgconf-2-4 libgnome-keyring0 libnspr4 libnss3 xdg-utils youtube-dl libappindicator1 libindicator7 coreutils libc6 libx11-6 x11-common xauth xterm xinit lxterminal evince omxplayer uget tint2 vlc libpcre3 fonts-freefont-ttf fbset
+sudo apt-get -y install gdebi gconf-service libgconf-2-4 libgnome-keyring0 libnspr4 libnss3 xdg-utils youtube-dl libappindicator1 libindicator7 coreutils libc6 libx11-6 x11-common xauth xterm xinit lxterminal evince omxplayer uget tint2 vlc libpcre3 fonts-freefont-ttf fbset openbox ratpoison
 sudo apt-get --purge --reinstall -y install pulseaudio
 sudo apt-get -y install omxplayer || wget https://dl.dropboxusercontent.com/u/33674660/Raspberry%20Pi%202/omxplayer_0.3.7-git20160206-cb91001_armhf.deb && sudo dpkg -i omxplayer_0.3.7~git20160206~cb91001_armhf.deb
 clear && echo $"Installing Browser...\n"
