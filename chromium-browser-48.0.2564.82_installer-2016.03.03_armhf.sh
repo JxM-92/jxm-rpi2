d5cdf5b0d7681b742fec6d74bd58e25edaa3d224
Joe X <jamedinao@gmail.com>
2016-03-04 16:36:18 -0500

    Update chromium-browser-48.0.2564.82_installer-2016.03.03_armhf.sh

--- chromium-browser-48.0.2564.82_installer-2016.03.03_armhf.sh
+++ chromium-browser-48.0.2564.82_installer-2016.03.03_armhf.sh
@@ -1,20 +1,17 @@
 #/bin/bash
 # More info at: https://www.raspberrypi.org/forums/viewtopic.php?t=121195
 # Script created by Joe X (JxM S.A.)
+echo $"Downloading Browser...\n"
 wget https://dl.dropboxusercontent.com/u/87113035/chromium-browser-l10n_48.0.2564.82-0ubuntu0.15.04.1.1193_all.deb
 wget https://dl.dropboxusercontent.com/u/87113035/chromium-browser_48.0.2564.82-0ubuntu0.15.04.1.1193_armhf.deb
 wget https://dl.dropboxusercontent.com/u/87113035/chromium-codecs-ffmpeg-extra_48.0.2564.82-0ubuntu0.15.04.1.1193_armhf.deb
 wget http://steinerdatenbank.de/software/kweb-1.6.9.tar.gz
-sudo apt-get -y install gdebi gconf-service libgconf-2-4 libgnome-keyring0 libnspr4 libnss3 xdg-utils youtube-dl libappindicator1 libindicator7 coreutils libc6 libx11-6 x11-common xauth xterm xinit
+echo $"Installing Dependencies...\n"
+sudo apt-get -y install gdebi gconf-service libgconf-2-4 libgnome-keyring0 libnspr4 libnss3 xdg-utils youtube-dl libappindicator1 libindicator7 coreutils libc6 libx11-6 x11-common xauth xterm xinit lxterminal evince omxplayer uget tint2 vlc
+echo $"Installing Browser...\n"
 tar -xzf kweb-1.6.9.tar.gz
 cd kweb-1.6.9
 sudo ./debinstall
 cd ..
 sudo dpkg -i chromium-codecs-ffmpeg-extra_48.0.2564.82-0ubuntu0.15.04.1.1193_armhf.deb
 sudo dpkg -i chromium-browser-l10n_48.0.2564.82-0ubuntu0.15.04.1.1193_all.deb chromium-browser_48.0.2564.82-0ubuntu0.15.04.1.1193_armhf.deb
-echo "Do you want to start Chromium-Browser? (Y/N)"
-read answer
-case answer in
-  [Y|y]) xinit -bg black -fg white chromium-browser ;;
-  *) break ;;
-esac
