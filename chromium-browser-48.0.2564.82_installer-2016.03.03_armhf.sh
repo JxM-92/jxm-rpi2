295c4dfd3a7e414e52bb45dfcc71a8a61bd75e79
Joe X <jamedinao@gmail.com>
2016-03-04 14:16:57 -0500

    Update chromium-browser-48.0.2564.82_installer-2016.03.03_armhf.sh

--- chromium-browser-48.0.2564.82_installer-2016.03.03_armhf.sh
+++ chromium-browser-48.0.2564.82_installer-2016.03.03_armhf.sh
@@ -4,6 +4,6 @@
 wget https://dl.dropboxusercontent.com/u/87113035/chromium-browser-l10n_48.0.2564.82-0ubuntu0.15.04.1.1193_all.deb
 wget https://dl.dropboxusercontent.com/u/87113035/chromium-browser_48.0.2564.82-0ubuntu0.15.04.1.1193_armhf.deb
 wget https://dl.dropboxusercontent.com/u/87113035/chromium-codecs-ffmpeg-extra_48.0.2564.82-0ubuntu0.15.04.1.1193_armhf.deb
-sudo apt-get -y install gconf-service libgconf-2-4 libgnome-keyring0 libnspr4 libnss3 xdg-utils youtube-dl coreutils libc6 libx11-6 x11-common xauth xserver-xorg xterm xinit
+sudo apt-get -y install gconf-service libgconf-2-4 libgnome-keyring0 libnspr4 libnss3 xdg-utils youtube-dl coreutils libc6 libx11-6 x11-common xauth xterm xinit
 sudo dpkg -i chromium-codecs-ffmpeg-extra_48.0.2564.82-0ubuntu0.15.04.1.1193_armhf.deb
 sudo dpkg -i chromium-browser-l10n_48.0.2564.82-0ubuntu0.15.04.1.1193_all.deb chromium-browser_48.0.2564.82-0ubuntu0.15.04.1.1193_armhf.deb
