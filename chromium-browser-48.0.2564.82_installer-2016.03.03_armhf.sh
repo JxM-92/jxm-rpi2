b4e0361cb8d55323ef451c0f1bc8f3997fe7f053
Joe X <jamedinao@gmail.com>
2016-03-04 17:02:46 -0500

    Update chromium-browser-48.0.2564.82_installer-2016.03.03_armhf.sh

--- chromium-browser-48.0.2564.82_installer-2016.03.03_armhf.sh
+++ chromium-browser-48.0.2564.82_installer-2016.03.03_armhf.sh
@@ -7,8 +7,9 @@ wget https://dl.dropboxusercontent.com/u/87113035/chromium-browser_48.0.2564.82-
 wget https://dl.dropboxusercontent.com/u/87113035/chromium-codecs-ffmpeg-extra_48.0.2564.82-0ubuntu0.15.04.1.1193_armhf.deb
 wget http://steinerdatenbank.de/software/kweb-1.6.9.tar.gz
 clear && echo $"Installing Dependencies...\n"
-sudo apt-get update && sudo apt-get -y install gdebi gconf-service libgconf-2-4 libgnome-keyring0 libnspr4 libnss3 xdg-utils youtube-dl libappindicator1 libindicator7 coreutils libc6 libx11-6 x11-common xauth xterm xinit lxterminal evince omxplayer uget tint2
-sudo apt-get -y install omxplayer vlc || sudo apt-get -y install vlc
+sudo apt-get update && sudo apt-get -y upgrade
+sudo apt-get -y install gdebi gconf-service libgconf-2-4 libgnome-keyring0 libnspr4 libnss3 xdg-utils youtube-dl libappindicator1 libindicator7 coreutils libc6 libx11-6 x11-common xauth xterm xinit lxterminal evince omxplayer uget tint2 vlc
+sudo apt-get -y install omxplayer || wget http://omxplayer.sconde.net/builds/omxplayer_0.3.7~git20160206~cb91001_armhf.deb && sudo dpkg -i omxplayer_0.3.7~git20160206~cb91001_armhf.deb
 clear && echo $"Installing Browser...\n"
 tar -xzf kweb-1.6.9.tar.gz
 cd kweb-1.6.9
